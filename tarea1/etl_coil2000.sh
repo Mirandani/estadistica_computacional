43fbc0b58698d821ef0975ed2ca7bbd0a7215850
Mirandani <>
2025-09-13 19:01:06 -0600

    [ADD] unzip file

---
+++ tarea1/etl_coil2000.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# ETL script for Coil2000 dataset
+wget https://archive.ics.uci.edu/static/public/125/insurance+company+benchmark+coil+2000.zip
+unzip insurance+company+benchmark+coil+2000.zip
+
